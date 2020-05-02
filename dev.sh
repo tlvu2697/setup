0196b0cdd155ec9a78c7ce9deeaf34276009aee4
Vu Tran <vu.tran@employmenthero.com>
2020-05-02 11:43:24 +0700

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -21,8 +21,8 @@ brew install twpayne/taps/chezmoi
 echo "[*] Install Nodejs..."
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-asdf install nodejs 10.15.0
-asdf global nodejs 10.15.0
+asdf install nodejs 13.14.0
+asdf global nodejs 13.14.0
 
 echo "[*] Install Python..."
 asdf plugin-add python
