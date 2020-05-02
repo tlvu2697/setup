c3a030c2c34281d8e627571f20117cc495807077
Vu Tran <vu.tran@employmenthero.com>
2020-05-02 17:10:25 +0700

    Update ubuntu-dep.sh

--- ubuntu-dep.sh
+++ ubuntu-dep.sh
@@ -4,7 +4,10 @@ echo "[*] Installing system utils..."
 sudo apt-get install -y gnome-tweaks gnome-panel ppa-purge xclip ctags file make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git font-manager ripgrep
 
 echo "[*] Init SSH key"
-ssh-keygen -t rsa -b 4096 -C "tlvu2697@gmail.com"	
+ssh-keygen -t rsa -b 4096 -C "tlvu2697@gmail.com"
+
+echo "[*] Update persmission on /usr/local/bin"
+sudo chown -R $(whoami) /usr/local/bin
 
 echo "[*] Installing ibus..."
 sudo apt install ibus
