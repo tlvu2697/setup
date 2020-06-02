1905949ae895bb775679100d871320e81a9c9211
Le-Vu Tran <vu.tran@employmenthero.com>
2020-06-02 14:44:07 +0700

    Update imagemagick.sh

--- imagemagick.sh
+++ imagemagick.sh
@@ -6,4 +6,4 @@ brew link --force imagemagick
 
 echo "[*] Create system symlink"
 locate libMagickCore-7.Q16HDRI.so.7
-sudo ln -s /home/linuxbrew/.linuxbrew/Cellar/imagemagick/7.0.10-10/lib/libMagickCore-7.Q16HDRI.so.7 /usr/lib/x86_64-linux-gnu/libMagickCore-7.Q16HDRI.so.7
+sudo ln -s /home/linuxbrew/.linuxbrew/lib/libMagickCore-7.Q16HDRI.so.7 /usr/lib/x86_64-linux-gnu/libMagickCore-7.Q16HDRI.so.7
