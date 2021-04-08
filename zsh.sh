c3767b25e1eb8c1f11a1668c21d0c752ca1aeea0
Le-Vu Tran <vu.tran@employmenthero.com>
2021-04-08 17:08:43 +0700

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -2,6 +2,11 @@
 
 echo "[*] Installing Zsh..."
 sudo apt-get install -y zsh
+chsh -s /bin/zsh
 
-echo "[*] Installing Oh-My-Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "[*] Installing Prezto..."
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
