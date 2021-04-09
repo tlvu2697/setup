cfeb4e40410d0687f0df95d57f979c992f0e8884
Le-Vu Tran <vu.tran@employmenthero.com>
2021-04-09 10:59:02 +0700

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -4,9 +4,5 @@ echo "[*] Installing Zsh..."
 sudo apt-get install -y zsh
 chsh -s /bin/zsh
 
-echo "[*] Installing Prezto..."
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+echo "[*] Installing zinit..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
