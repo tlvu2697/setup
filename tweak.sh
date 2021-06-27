ae11cbe4b95e6eb92d4bf3c638ba33c1ee612443
Le-Vu Tran <vu.tran@employmenthero.com>
2021-06-27 15:20:56 +0700

    Update tweak.sh

--- tweak.sh
+++ tweak.sh
@@ -1,6 +1,9 @@
 echo "[*] Update persmission on /usr/local/bin"
 sudo chown -R $(whoami) /usr/local/bin
 
+echo "[*] Update persmission on /var/log"
+sudo chown -R $(whoami) /var/log
+
 echo "[*] Change swap size to 32GB (https://bogdancornianu.com/change-swap-size-in-ubuntu/)"
 sudo swapoff -a
 sudo dd if=/dev/zero of=/swapfile bs=1G count=32
@@ -15,5 +18,5 @@ sudo sysctl vm.swappiness=10
 echo "[*] Increase max_user_watches (https://github.com/google/cadvisor/issues/1581)"
 sudo sysctl fs.inotify.max_user_watches=1048576
 
-echo "[*] Deel sleep (https://askubuntu.com/questions/1029474/ubuntu-18-04-dell-xps13-9370-no-longer-suspends-on-lid-close)"
+echo "[*] Deep sleep (https://askubuntu.com/questions/1029474/ubuntu-18-04-dell-xps13-9370-no-longer-suspends-on-lid-close)"
 echo deep | sudo tee /sys/power/mem_sleep
