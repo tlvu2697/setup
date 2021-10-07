7b2cd76df7e984a312cc06fb54973f27ad495e00
Le-Vu Tran <vu.tran@employmenthero.com>
2021-10-07 11:09:27 +0700

    Update ubuntu-dep.sh

--- ubuntu-dep.sh
+++ ubuntu-dep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[*] Installing system utils..."
-sudo apt install -y gnome-tweaks xclip exuberant-ctags build-essential wget curl git font-manager ripgrep tmux mlocate
+sudo apt install -y gnome-tweaks xclip exuberant-ctags build-essential wget curl git font-manager ripgrep tmux mlocate imwheel
 
 echo "[*] Python build environment"
 sudo apt-get install make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
