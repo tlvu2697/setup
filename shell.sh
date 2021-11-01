fc9c437abd85cf01f88896a5c8623eaf1d0b9ae5
Le-Vu Tran <vu.tran@employmenthero.com>
2021-11-01 14:38:38 +0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -4,8 +4,11 @@ echo "[*] Installing Zsh..."
 sudo apt-get install -y zsh
 sudo chsh -s $(which zsh)
 
-echo "[*] Installing zinit..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+
+echo "[*] Installing OhMyZsh"
+
+echo "[*] Installing OhMyZsh Plugins"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "[*] Install fzf..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
