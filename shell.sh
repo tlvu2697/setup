743087e050bb0a4db4793996fd3e15f465777cef
Le-Vu Tran <vu.tran@employmenthero.com>
2021-11-01 14:38:58 +0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -4,8 +4,8 @@ echo "[*] Installing Zsh..."
 sudo apt-get install -y zsh
 sudo chsh -s $(which zsh)
 
-
 echo "[*] Installing OhMyZsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "[*] Installing OhMyZsh Plugins"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
