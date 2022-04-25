da3afb4ce3b0dea477aa44ade8230411501b2d68
Le-Vu Tran <vu.tran@employmenthero.com>
2022-04-25 23:48:07 +0700

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -29,10 +29,10 @@ asdf global nodejs 16.14.2
 
 echo "[*] Installing Python..."
 asdf install python 2.7.18
-asdf install python 3.6.15
-asdf global python 3.6.15 2.7.18
-sudo ln -s ~/.asdf/shims/python3 /usr/local/bin/_python3.6
-sudo ln -s ~/.asdf/shims/python2 /usr/local/bin/_python2.7
+asdf install python 3.4.10
+asdf global python 3.4.10 2.7.18
+sudo ln -s ~/.asdf/shims/python3 ~/.local/bin/_python3.6
+sudo ln -s ~/.asdf/shims/python2 ~/.local/bin/_python2.7
 
 echo "[*] Installing Ruby..."
 asdf install ruby 3.0.2
@@ -45,25 +45,17 @@ pip2 install neovim
 pip3 install neovim
 npm install -g neovim
 gem install neovim
-sudo ln -s ~/.asdf/shims/neovim-ruby-host /usr/local/bin/_neovim-ruby-host
+sudo ln -s ~/.asdf/shims/neovim-ruby-host ~/.local/bin/_neovim-ruby-host
 
 echo "[*] Installing Tmuxinator..."
 gem install tmuxinator
-sudo ln -s ~/.asdf/installs/ruby/3.0.2/bin/tmuxinator /usr/local/bin/muxinator
+sudo ln -s ~/.asdf/installs/ruby/3.0.2/bin/tmuxinator ~/.local/bin/muxinator
 
 echo "[*] Installing LazyGit..."
-sudo add-apt-repository ppa:lazygit-team/release
-sudo apt-get update
-sudo apt-get install lazygit
+MY_FLAVOR=Linux_x86_64; curl -s -L $(curl -s https://api.github.com/repos/jesseduffield/lazygit/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep -i "$MY_FLAVOR") | sudo tar xzf - -C ~/.local/bin lazygit
 
 echo "[*] Installing LazyDocker..."
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-
-echo "[*] Installing Git Cola"
-git clone git://github.com/git-cola/git-cola.git ~/.git-cola
-pip install pyqt5
-sudo ln -s ~/.git-cola/bin/git-dag /usr/local/bin/git-dag
-sudo ln -s ~/.git-cola/bin/git-cola /usr/local/bin/git-cola
 
 echo "[*] Redis Installation"
 echo "https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-redis-on-ubuntu-20-04"
@@ -73,3 +65,11 @@ echo "https://www.digitalocean.com/community/tutorials/how-to-install-and-use-po
 
 echo "[*] Docker Installation"
 echo "https://docs.docker.com/engine/install/ubuntu/"
+
+
+# DEPRECATED
+echo "[*] Installing Git Cola"
+git clone git://github.com/git-cola/git-cola.git ~/.git-cola
+pip install pyqt5
+sudo ln -s ~/.git-cola/bin/git-dag /usr/local/bin/git-dag
+sudo ln -s ~/.git-cola/bin/git-cola /usr/local/bin/git-cola
