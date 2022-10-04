27b2bd5509b20765f6bc89cc816cc2156f5a142d
Le-Vu Tran <vu.tran@employmenthero.com>
2022-10-04 09:25:44 +0700

    Add macos dep

---
+++ macos/dep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "[*] Init SSH key"
+ssh-keygen -t ed25519 -C "tlvu2697@gmail.com"
+
+echo "[*] Installing system utils..."
+brew install xclip ctags wget curl git ripgrep tmux fzf fd
+
+echo "[*] Installing apps..."
+brew install --cask beekeeper-studio mailstring notion postman slack spotify telegram
